dcf972129a61bbb4ed0fabf28958c688be7a1df0
johnta0 <22534726+johnta0@users.noreply.github.com>
2022-09-20 18:55:52 +0900

    Return 404 when a file is not found

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -9,9 +9,21 @@ export default async function handler(req, res) {
 
   //Find the absolute path of the json directory
   const jsonDirectory = path.join(process.cwd(), 'json');
+
   //Read the json data file data.json
-  let fileContents = await fs.readFile(`${jsonDirectory}/data.json`);
-  if (channel_name && date) fileContents = await fs.readFile(`${jsonDirectory}/${channel_name}/${date}.json`, 'utf8');
+  let fileContents;
+  try {
+    fileContents = await fs.readFile(`${jsonDirectory}/data.json`);
+    if (channel_name && date) fileContents = await fs.readFile(`${jsonDirectory}/${channel_name}/${date}.json`, 'utf8');
+  } catch (e) {
+    console.log("Error:", e.code);
+    if (e.code == 'ENOENT') {
+      res.status(404).json({
+          error:  "No messages for specified channelName and date found."
+      });
+    }
+    res.status(500).json({ error: "Internal server error." });
+  }
   //Return the content of the data file in json format
   res.status(200).json(JSON.parse(fileContents));
 }
